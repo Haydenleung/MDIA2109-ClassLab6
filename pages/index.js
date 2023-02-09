0c97c4a5020fba5094e4956b1e9de6ab91b165e6
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-02-09 14:45:18 -0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Survey from './Survey'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Survey />
+    </>
+  )
+}
